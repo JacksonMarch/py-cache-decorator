142771af7e43933e317316a6439125dab9cbd240
Yevgeniy Marchenko <Jackson.stethem@gmail.com>
2026-03-17 23:24:05 +0200

    Add cache function with all rules

--- app/main.py
+++ app/main.py
@@ -1,6 +1,18 @@
-from typing import Callable
+from typing import Callable, Any
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    storage: dict[tuple[Any, ...], Any] = {}
+    @wraps(func)
+    def wrapper(*args: Any, **kwargs: Any) -> Any:
+        key = (*args, tuple(sorted(kwargs.items())))
+        if key in storage:
+            print("Getting from cache")
+            return storage[key]
+        print("Calculating new result")
+        result = func(*args, **kwargs)
+        storage[key] = result
+        return result
+
+    return wrapper
